d5853ac7827f3ec7a43f543c90bad591059988ac
Rayne <pluvstream@gmail.com>
2026-02-27 23:11:22 +0900

    260227

---
+++ src/main/java/dev/coma/study/chap11/DriverTest.java
@@ -0,0 +1,19 @@
+package dev.coma.study.chap11;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DriverTest {
+  public static void main(String[] args) {
+    Connection con;
+
+    try {
+      Class.forName("org.mariadb.jdbc.Driver").getDeclaredConstructor().newInstance();
+      // 연습용 DB user account
+      con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/forJSP", "forJSP", "0000");
+      System.out.println("Success");
+    } catch (Exception e) {
+      e.printStackTrace(System.err);
+    }
+  }
+}
